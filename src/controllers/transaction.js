ab988977917e4a7dd715ac2fb89a7250aca9b6c6
Kev2Cod <kevinity310@gmail.com>
2022-07-03 04:09:08 -0400

    make it better

--- src/controllers/transaction.js
+++ src/controllers/transaction.js
@@ -296,84 +296,84 @@ const updateProduct = async (orderId) => {
 };
 
 // Handle send email
-const sendEmail = async (status, transactionId) => {
-  // Config service and email account
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.SYSTEM_EMAIL,
-      pass: process.env.SYSTEM_PASSWORD,
-    },
-  });
+// const sendEmail = async (status, transactionId) => {
+//   // Config service and email account
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.SYSTEM_EMAIL,
+//       pass: process.env.SYSTEM_PASSWORD,""
+//     },
+//   });
 
-  // Get transaction data
-  let data = await transaction.findOne({
-    where: {
-      id: transactionId,
-    },
-    attributes: {
-      exclude: ["createdAt", "updatedAt", "password"],
-    },
-    include: [
-      {
-        model: user,
-        as: "buyer",
-        attributes: {
-          exclude: ["createdAt", "updatedAt", "password", "status"],
-        },
-      },
-      {
-        model: product,
-        as: "product",
-        attributes: {
-          exclude: ["createdAt", "updatedAt", "idUser", "qty", "price", "desc"],
-        },
-      },
-    ],
-  });
+//   // Get transaction data
+//   let data = await transaction.findOne({
+//     where: {
+//       id: transactionId,
+//     },
+//     attributes: {
+//       exclude: ["createdAt", "updatedAt", "password"],
+//     },
+//     include: [
+//       {
+//         model: user,
+//         as: "buyer",
+//         attributes: {
+//           exclude: ["createdAt", "updatedAt", "password", "status"],
+//         },
+//       },
+//       {
+//         model: product,
+//         as: "product",
+//         attributes: {
+//           exclude: ["createdAt", "updatedAt", "idUser", "qty", "price", "desc"],
+//         },
+//       },
+//     ],
+//   });
 
-  data = JSON.parse(JSON.stringify(data));
+//   data = JSON.parse(JSON.stringify(data));
 
-  // Email options content
-  const mailOptions = {
-    from: process.env.SYSTEM_EMAIL,
-    to: data?.buyer.email,
-    subject: "Payment status",
-    text: "Your payment is <br />" + status,
-    html: `<!DOCTYPE html>
-            <html lang="en">
-              <head>
-                <meta charset="UTF-8" />
-                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>Document</title>
-                <style>
-                  h1 {
-                    color: brown;
-                  }
-                </style>
-              </head>
-              <body>
-                <h2>Product payment :</h2>
-                <ul style="list-style-type:none;">
-                  <li>Name : ${data?.product?.name}</li>
-                  <li>Total payment: ${data?.price}</li>
-                  <li>Status : <b>${status}</b></li>
-                </ul>  
-              </body>
-            </html>`,
-  };
+//   // Email options content
+//   const mailOptions = {
+//     from: process.env.SYSTEM_EMAIL,
+//     to: data?.buyer.email,
+//     subject: "Payment status",
+//     text: "Your payment is <br />" + status,
+//     html: `<!DOCTYPE html>
+//             <html lang="en">
+//               <head>
+//                 <meta charset="UTF-8" />
+//                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+//                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//                 <title>Document</title>
+//                 <style>
+//                   h1 {
+//                     color: brown;
+//                   }
+//                 </style>
+//               </head>
+//               <body>
+//                 <h2>Product payment :</h2>
+//                 <ul style="list-style-type:none;">
+//                   <li>Name : ${data?.product?.name}</li>
+//                   <li>Total payment: ${data?.price}</li>
+//                   <li>Status : <b>${status}</b></li>
+//                 </ul>  
+//               </body>
+//             </html>`,
+//   };
 
-  // Send an email if there is a change in the transaction status
-  if (data.status != status) {
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) throw err;
-      console.log("Email sent: " + info.response);
+//   // Send an email if there is a change in the transaction status
+//   if (data?.status !== status) {
+//     transporter.sendMail(mailOptions, (err, info) => {
+//       if (err) throw err;
+//       console.log("Email sent: " + info.response);
 
-      return res.send({
-        status: "Success",
-        message: info.response,
-      });
-    });
-  }
-};
+//       return res.send({
+//         status: "Success",
+//         message: info.response,
+//       });
+//     });
+//   }
+// };
