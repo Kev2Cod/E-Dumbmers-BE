df4e9dd028ad04580635b54a9ef03ab02845bae0
Kev2Cod <kevinity310@gmail.com>
2022-08-18 06:04:27 +0700

    fix: connect database postgress

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/v1/", router);
+app.use("/api/", router);
 app.use("/uploads", express.static("uploads"));
 
 // Ganti App menjadi Server
