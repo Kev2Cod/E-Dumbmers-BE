47825465f2ca81562d9b1d7bf4e96247c5cb22c8
Kev2Cod <kevinity310@gmail.com>
2022-08-18 07:21:53 +0700

    update

--- models/index.js
+++ models/index.js
@@ -8,10 +8,10 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 console.log(config);
 const db = {};
-
+console.log(config.use_env_variable);
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(config.use_env_variable, config);
 } else {
   sequelize = new Sequelize(
     config.database,
